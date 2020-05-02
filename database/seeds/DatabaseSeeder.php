7c6abd6c75174a6abb1c2d46684f40e850dc31e2
canario <45767956+Laddergrowth@users.noreply.github.com>
2020-05-02 17:24:13 +0100

    database
    connections

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,10 +7,22 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      *
-     * @return void
-     */
+     //* @return void
+     **/
     public function run()
     {
-        // $this->call(UserSeeder::class);
+
+        factory(App\User::class, 10)->create(); // ->each(function($user){
+
+
+//             $user->posts()->save(factory(App\Post::class)->make());
+
+
+//       });
+
+
+         $this->call(UserSeeder::class);
     }
 }
+
+
